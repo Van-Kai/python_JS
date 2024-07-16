9e73d19af9588af67498053443f604a389bcdec7
Van-Kai <15565315419@163.com>
2024-07-16 09:21:10 +0800

    Update Python_JS.py

--- Python_JS.py
+++ Python_JS.py
@@ -1,8 +1,19 @@
 import time
 import subprocess
-    def main():
-        while True:
-            result = subprocess.run(["node", "-e", 'console.log("kaikai")'], capture_output=True, text=True)
-            print(result.stdout)
-            time.sleep(1)
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello():
+    result = subprocess.run(["node", "-e", 'console.log("kaikai")'], capture_output=True, text=True)
+    return result.stdout
+ 
+if __name__ == '__main__':
+    app.run()
+    # def main():
+    #     while True:
+    #         result = subprocess.run(["node", "-e", 'console.log("kaikai")'], capture_output=True, text=True)
+    #         print(result.stdout)
+    #         time.sleep(1)
 
