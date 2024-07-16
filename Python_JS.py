d96eb5cfa51e321b0e172714a6e9b0803d7c3260
Van-Kai <15565315419@163.com>
2024-07-16 09:46:05 +0800

    Update Python_JS.py

--- Python_JS.py
+++ Python_JS.py
@@ -1,16 +1,10 @@
-import time
+
 import subprocess
+
+
 def handler():
     result = subprocess.run(["node", "-e", 'console.log("kaikai")'], capture_output=True, text=True).stdout
-
     return {
         'statusCode': 200,
         'body': result
     }
-
-    # def main():
-    #     while True:
-    #         result = subprocess.run(["node", "-e", 'console.log("kaikai")'], capture_output=True, text=True)
-    #         print(result.stdout)
-    #         time.sleep(1)
-
